829ba976aaba989b2a4e1cdd593b99a9a3f881d7
Kelvin Checo <checokelvin@gmail.com>
2018-09-25 02:57:00 -0400

    Defined Todo model

---
+++ app/Models/Todo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'todos';
+
+
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'task', 
+    	'completed',
+    ];
+}
