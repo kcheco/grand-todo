e0db4e7bf4054bc112fe56b168fea1206e6cd4dc
Kelvin Checo <checokelvin@gmail.com>
2018-09-27 23:18:24 -0400

    Created a model to log api requests and migrated to DB

---
+++ app/Models/Request.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'request_logs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'http_status_code', 'request_method', 'route'
+    ];
+}
