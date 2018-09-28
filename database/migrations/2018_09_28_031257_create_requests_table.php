bf789efe32b6ca99bd2f0901a3c1a40b99e02b05
Kelvin Checo <checokelvin@gmail.com>
2018-09-28 10:36:15 -0400

    Fixed bug with drop function in request logs table migration file

--- database/migrations/2018_09_28_031257_create_requests_table.php
+++ database/migrations/2018_09_28_031257_create_requests_table.php
@@ -29,6 +29,6 @@ class CreateRequestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('requests');
+        Schema::dropIfExists('request_logs');
     }
 }
