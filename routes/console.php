17a91a9fa977de038b63cb5c6bf5c9dbf5e594a5
Kelvin Checo <checokelvin@gmail.com>
2018-09-28 00:42:43 -0400

    Added an artisan command for quick access to request logs

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Request;
 use Illuminate\Foundation\Inspiring;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+
+Artisan::command('request_log', function() {
+	$this->comment(Request::all());
+})->describe('Display a list of all API requests');
